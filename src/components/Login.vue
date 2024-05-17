afc179890bf345bc6fe898a1ea251e2e79ff33f4
Neoxy <1007050740@qq.com>
2024-05-17 22:54:56 +0800

    add Login.vue

---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { ElButton } from 'element-plus'
+  export default {
+    components: { ElButton },
+  }
+</script>
+
+<template>
+    <el-button>count is: </el-button>
+</template>
+
+<style scoped>
+
+</style>
